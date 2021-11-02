66756423b6cc52020b85a25ce27b1f1505e75bce
Liam Colaluca <91563054+colalucal2024@users.noreply.github.com>
2021-11-02 18:40:53 +0000

    added movement

--- student.py
+++ student.py
@@ -57,7 +57,7 @@ class Piggy(PiggyParent):
     ****************
     '''
     def liam(self):
-     pass
+     deg_fwd(360)
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
