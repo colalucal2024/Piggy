5c620bb68fcb0f132735584e839970f66750ddf8
Liam Colaluca <91563054+colalucal2024@users.noreply.github.com>
2021-12-09 19:19:50 +0000

    sleep

--- student.py
+++ student.py
@@ -65,6 +65,7 @@ class Piggy(PiggyParent):
       check_distance = 150
       while True:
         self.fwd()
+        time.sleep(5)
         if (self.read_distance() >= stopping_distance):
           self.left()
           time.sleep(.75)
